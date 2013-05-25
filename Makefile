361559ab1353fa7245a331fca57bd7f2d0578dca
Piotr Esden-Tempski <piotr@esden.net>
2013-05-25 15:42:56 -0700

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,61 @@
+##
+## This file is part of the superbitrf project.
+##
+## Copyright (C) 2013 Piotr Esden-Tempski <piotr@esden.net>
+##
+## This library is free software: you can redistribute it and/or modify
+## it under the terms of the GNU Lesser General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This library is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU Lesser General Public License for more details.
+##
+## You should have received a copy of the GNU Lesser General Public License
+## along with this library.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+TEST_TARGETS := test/blink
+
+# Be silent per default, but 'make V=1' will show all compiler calls.
+ifneq ($(V),1)
+Q := @
+# Do not print "Entering directory ...".
+MAKEFLAGS += --no-print-directory
+endif
+
+all: lib tests
+
+$(TEST_TARGETS): lib
+	@printf "  BUILD   $@\n";
+	$(Q)$(MAKE) --directory=$@
+
+tests: $(TEST_TARGETS)
+	$(Q)true
+
+
+lib:
+	$(Q)if [ ! "`ls -A libopencm3`" ] ; then \
+	echo "######## ERROR ########"; \
+	echo "\tlibopencm3 is not initialized."; \
+	echo "\tPlease run:"; \
+	echo "\t$$ git submodule init"; \
+	echo "\t$$ git submodule update"; \
+	echo "\tbefore running make."; \
+	echo "######## ERROR ########"; \
+	exit 1; \
+	fi
+	$(Q)$(MAKE) -C libopencm3
+
+clean:
+	$(Q)$(MAKE) -C libopencm3 clean
+	$(Q)for i in $(TEST_TARGETS); do \
+		if [ -d $$i ]; then \
+			printf "  CLEAN   $$i\n"; \
+			$(MAKE) -C $$i clean SRCLIBDIR=$(SRCLIBDIR) || exit $?; \
+		fi; \
+	done	
+
+.PHONY: all lib
---
+++ test/blink/blink.c
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the libopencm3 project.
+ *
+ * Copyright (C) 2009 Uwe Hermann <uwe@hermann-uwe.de>
+ *
+ * This library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libopencm3/stm32/f1/rcc.h>
+#include <libopencm3/stm32/f1/gpio.h>
+
+/* Set STM32 to 72 MHz. */
+static void clock_setup(void)
+{
+	//rcc_clock_setup_in_hse_12mhz_out_72mhz();
+
+	/* Enable GPIOC clock. */
+	rcc_peripheral_enable_clock(&RCC_APB2ENR, RCC_APB2ENR_IOPBEN);
+}
+
+static void gpio_setup(void)
+{
+	/* Set GPIO12 (in GPIO port C) to 'output push-pull'. */
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO2);
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO10);
+	gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+		      GPIO_CNF_OUTPUT_PUSHPULL, GPIO11);
+}
+
+int main(void)
+{
+	int i;
+
+	clock_setup();
+	gpio_setup();
+	gpio_set(GPIOB, GPIO10);
+
+	/* Blink the LED (PC12) on the board. */
+	while (1) {
+		gpio_toggle(GPIOB, GPIO2);	/* LED on/off */
+		gpio_toggle(GPIOB, GPIO10);	/* LED on/off */
+		gpio_toggle(GPIOB, GPIO11);	/* LED on/off */
+		for (i = 0; i < 800000; i++)	/* Wait a bit. */
+			__asm__("nop");
+	}
+
+	return 0;
+}
